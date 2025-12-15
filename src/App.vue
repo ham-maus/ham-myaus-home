c42e1371a5390f96152329fd822afc9fa9c87b75
ham-maus <shirokuro2012@outlook.com>
2025-12-15 20:16:41 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+
+    <navbar />
+    <Main1 />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  import Main1 from './components/Main1.vue'
+  import Navbar from './components/Navbar.vue'
+
+  //
+</script>
---
+++ src/components/Main1.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-container class="pa-6 pa-md-12 my-16" fluid>
+<h1> hamのホームページ </h1>
+</v-container>
+</template>
