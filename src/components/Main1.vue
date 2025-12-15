fc694a5a09dba121403501ca408f0431a4aacd27
ham-maus <shirokuro2012@outlook.com>
2025-12-15 20:29:48 +0900

    Update Main1.vue

--- src/components/Main1.vue
+++ src/components/Main1.vue
@@ -1,5 +1,17 @@
 <template>
-  <v-container class="pa-6 pa-md-12 my-16" fluid>
-<h1> hamのホームページ </h1>
+<v-container class="pa-6 pa-md-12 my-16" fluid>
+  <p class="font-weight-medium text-primary">
+    hamは進化する（多分）
+  </p>
+
+  <h1 class="text-h3 font-weight-bold mt-2">
+    hamのホームページ
+  </h1>
+
+  <p class="mt-4 text-body-1 text-medium-emphasis">
+    よくわからないけど!!<br>
+    多分進んでいくことが大切なんだ!!
+  </p>
 </v-container>
+
 </template>
